70d4fd7e508f98dfc7972b1f400d3a383a0c3491
anson10 <sansonmsa@gmail.com>
2021-05-14 15:11:13 +0530

    New files added

--- files/readfile.py
+++ files/readfile.py
@@ -1,3 +1,12 @@
 with open("pifile.txt") as file_object:
-    content = file_object.read()
-    print(content)
+    lines = file_object.readlines()
+    pi_strings = ''
+
+    for line in lines:
+        pi_strings+=line.strip()
+
+birthday = input("Enter your bday in ddmmyy format :")
+if birthday in pi_strings:
+    print("Your bday is found in first million digits of pi")
+else:
+    print("Your bday does not appear in first million digits of pi")
